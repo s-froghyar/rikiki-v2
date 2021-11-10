386b3ae2bf3fe86cb91e987e4d3788a618cc6f0f
TheSoma300 <froghyar.soma@gmal.com>
2021-11-10 21:00:25 +0100

    initial commit

---
+++ apps/rikiki-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rikiki-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'rikiki-app';
+}
