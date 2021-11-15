6247a584b028c5da1eb26270e6b89758a52b8fe3
TheSoma300 <froghyar.soma@gmal.com>
2021-11-15 15:17:53 +0100

    refactoring auth app

--- apps/rikiki-auth/src/app/app.module.ts
+++ apps/rikiki-auth/src/app/app.module.ts
@@ -1,30 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+
+import { AuthService } from './auth.service';
 import { AppComponent } from './app.component';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { AuthService } from './auth.service';
-import { EmailComponent } from './email/email.component';
-import { ProfileComponent } from './profile/profile.component';
-import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LoginComponent, SignUpComponent, EmailComponent, ProfileComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignUpComponent,
+  ],
   imports: [
     BrowserModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
     BrowserAnimationsModule,
     MatProgressSpinnerModule,
-    AppRoutingModule
+    RouterModule.forRoot([{ path: '*', redirectTo: '', pathMatch: 'full' }])
   ],
   providers: [AuthService],
   bootstrap: [AppComponent],
--- apps/rikiki-auth/src/app/auth.service.ts
+++ apps/rikiki-auth/src/app/auth.service.ts
@@ -1,56 +1,126 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import firebase from 'firebase/app';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Injectable, NgZone } from '@angular/core';
+import { User } from "./shared/services/user.interface";
+import firebase from 'firebase/compat/app';
+import { AngularFireAuth } from "@angular/fire/compat/auth";
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { Router } from "@angular/router";
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
+
 export class AuthService {
+  userData: any; // Save logged in user data
 
   constructor(
-    private router: Router,
-    private readonly afAuth: AngularFireAuth) {}
-
-  login(email: string, password: string) {
-    this.afAuth.signInWithEmailAndPassword(email, password)
-    .then(value => {
-      console.log('Nice, it worked!');
-      this.router.navigateByUrl('/profile');
+    public afs: AngularFirestore,   // Inject Firestore service
+    public afAuth: AngularFireAuth, // Inject Firebase auth service
+    public router: Router,  
+    public ngZone: NgZone // NgZone service to remove outside scope warning
+  ) {    
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.userData = user;
+        localStorage.setItem('user', JSON.stringify(this.userData));
+        // JSON.parse(localStorage.getItem('user') ?? '');
+      } else {
+        localStorage.setItem('user', '');
+        // JSON.parse(localStorage.getItem('user') ?? '');
+      }
     })
-    .catch(err => {
-      console.log('Something went wrong: ', err.message);
-    });
   }
 
-  emailSignup(email: string, password: string) {
-    this.afAuth.createUserWithEmailAndPassword(email, password)
-    .then(value => {
-     console.log('Sucess', value);
-     this.router.navigateByUrl('/profile');
+  // Sign in with email/password
+  signIn(email: string, password: string) {
+    return this.afAuth.signInWithEmailAndPassword(email, password)
+      .then((result) => {
+        this.ngZone.run(() => {
+          this.router.navigate(['dashboard']);
+        });
+        this.setUserData(result.user);
+      }).catch((error) => {
+        window.alert(error.message)
+      })
+  }
+
+  // Sign up with email/password
+  signUp(email: string, password: string) {
+    return this.afAuth.createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        /* Call the SendVerificaitonMail() function when new user sign 
+        up and returns promise */
+        // this.sendVerificationMail();
+        this.setUserData(result.user);
+      }).catch((error) => {
+        window.alert(error.message)
+      })
+  }
+
+
+  // Reset Forggot password
+  forgotPassword(passwordResetEmail: string) {
+    return this.afAuth.sendPasswordResetEmail(passwordResetEmail)
+    .then(() => {
+      window.alert('Password reset email sent, check your inbox.');
+    }).catch((error) => {
+      window.alert(error)
     })
-    .catch(error => {
-      console.log('Something went wrong: ', error);
-    });
   }
 
-  googleLogin() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    return this.oAuthLogin(provider)
-      .then((value: any) => {
-     console.log('Sucess', value),
-     this.router.navigateByUrl('/profile');
-   })
-    .catch((error: any) => {
-      console.log('Something went wrong: ', error);
-    });
+  // Returns true when user is looged in and email is verified
+  get isLoggedIn(): boolean {
+    const user = JSON.parse(localStorage.getItem('user') ?? '');
+    return (user !== null && user.emailVerified !== false) ? true : false;
   }
 
-  logout() {
-    this.afAuth.signOut().then(() => {
-      this.router.navigate(['/']);
-    });
+  // Sign in with Google
+  googleAuth() {
+    return this.authLogin(new firebase.auth.GoogleAuthProvider());
   }
 
-  private oAuthLogin(provider: firebase.auth.GoogleAuthProvider): Promise<any> {
-    return this.afAuth.signInWithPopup(provider);
+  // Auth logic to run auth providers
+  authLogin(provider: firebase.auth.GoogleAuthProvider ) {
+    return this.afAuth.signInWithPopup(provider)
+      .then((result) => {
+        this.ngZone.run(() => {
+            this.router.navigate(['dashboard']);
+          })
+        this.setUserData(result.user);
+      }).catch((error) => {
+        window.alert(error)
+      })
   }
+
+  async sendVerificationMail() {
+    (await this.afAuth.currentUser)?.sendEmailVerification()
+      .then(() => {
+        this.router.navigate(['verify-email-address']);
+      })
+  }
+
+  /* Setting up user data when sign in with username/password, 
+  sign up with username/password and sign in with social auth  
+  provider in Firestore database using AngularFirestore + AngularFirestoreDocument service */
+  setUserData(user: any) {
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
+    const userData: User = {
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      emailVerified: user.emailVerified
+    }
+    return userRef.set(userData, {
+      merge: true
+    })
+  }
+
+  // Sign out 
+  signOut() {
+    return this.afAuth.signOut().then(() => {
+      localStorage.removeItem('user');
+      this.router.navigate(['sign-in']);
+    })
+  }
+
 }
