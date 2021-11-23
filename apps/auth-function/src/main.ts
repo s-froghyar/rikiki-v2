0f0d9d6514056835777f7a6035a1bfdc2b7ff054
TheSoma300 <froghyar.soma@gmal.com>
2021-11-23 08:27:58 +0100

    first release for the pipeline to work

---
+++ apps/auth-function/src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app/app.module';
+import express from 'express';
+import * as functions from 'firebase-functions';
+
+const server = express();
+
+export const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+
+  return app.init();
+};
+
+createNestServer(server)
+    .then(v => console.log('Nest Ready'))
+    .catch(err => console.error('Nest broken', err));
+
+// Connect express server to Firebase Functions
+export const authFun = functions.region('europe-west1').https.onRequest(server);
